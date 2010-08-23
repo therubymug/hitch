affb7d7dd34d5f96c2ee021641f4b113fb40b8d2
Rogelio J. Samour <rogelio@therubymug.com>
2010-08-22 22:47:48 -0400

    Stub out all files as Tempfiles for specs

--- spec/helper.rb
+++ spec/helper.rb
@@ -4,3 +4,11 @@ require 'fileutils'
 require 'tempfile'
 
 require File.join(File.dirname(__FILE__), '..', 'lib', 'hitch')
+
+Spec::Runner.configure do |config|
+  config.before(:each) do
+    Hitch.stub(:hitchrc).and_return(Tempfile.new('hitchrc').path)
+    Hitch.stub(:hitch_export_authors).and_return(Tempfile.new('hitch_export_authors').path)
+    Hitch::Author.stub(:hitch_pairs).and_return(Tempfile.new('hitch_pairs').path)
+  end
+end
