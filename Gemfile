774ebdac423d490e0c99e708a209a0ff3c2fe6ea
Rogelio J. Samour <rogelio@therubymug.com>
2011-08-30 22:50:17 -0400

    Update ruby-debug

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
-source :gemcutter
+source 'http://rubygems.org'
 
 gem 'highline', '1.5.0'
 gem 'jeweler', '1.4.0'
 gem 'rspec', '1.3.1'
-gem 'ruby-debug' if RUBY_VERSION =~ /^1.8/
+unless RUBY_VERSION =~ /^1.8.6/
+  gem 'ruby-debug', :require => 'ruby-debug', :platform => 'ruby_18'
+  gem 'ruby-debug19', :require => 'ruby-debug', :platform => 'ruby_19'
+end
--- rake_spec_with_all_rubies.sh
+++ rake_spec_with_all_rubies.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  source "$HOME/.rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+fi
+
 rubies=( 1.8.6 1.8.7 1.9.2 )
 gemset="hitch"
 
-for x in ${rubies[*]}; do rvm use --create $x@$gemset && bundle install; done
-for x in ${rubies[*]}; do rvm use $x@$gemset && rake; done
+for x in ${rubies[*]}; do
+  rvm use --create $x@$gemset
+  if ! command -v bundle >/dev/null; then
+    gem install bundler
+  fi
+  bundle install | grep -v 'Using' | grep -v 'complete' | sed '/^$/d'
+  rake spec
+done
