671c81c3512065b8361e21134a2c45d8ae3e7caf
Andre Helberg <helberg.andre@gmail.com>
2015-02-15 09:48:06 +0200

    Removed depricated stup methods in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,9 +8,4 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'hitch')
 RSpec.configure do |config|
   config.color = true
   config.formatter = 'progress'
-  config.before(:each) do
-    Hitch.stub(:hitchrc).and_return(Tempfile.new('hitchrc').path)
-    Hitch.stub(:hitch_export_authors).and_return(Tempfile.new('hitch_export_authors').path)
-    Hitch::Author.stub(:hitch_pairs).and_return(Tempfile.new('hitch_pairs').path)
-  end
 end
