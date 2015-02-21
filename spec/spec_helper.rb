cae14a2347365147f3e65d27f5f927b1f2ae795e
Andre Helberg <helberg.andre@gmail.com>
2015-02-21 16:53:19 +0200

    Fixed require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'fileutils'
 require 'tempfile'
 require 'pry'
 
-require File.join(File.dirname(__FILE__), '..', 'lib', 'hitch')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'git-pair')
 
 RSpec.configure do |config|
   config.color = true
