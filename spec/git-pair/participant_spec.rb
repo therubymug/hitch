90e532bc68b895572ce6bd1daa1c778b3b373e6d
Andre Helberg <helberg.andre@gmail.com>
2015-02-22 18:33:46 +0200

    Foxed participant spec

--- spec/git-pair/participant_spec.rb
+++ spec/git-pair/participant_spec.rb
@@ -1,32 +1,29 @@
 require 'spec_helper'
 
-describe Hitch::Participant do
+describe GitPair::Participant do
 
   let(:base_pairs) do
     {
       'bender' => {"email"=>"rcrichoch@fakeinbox.com", "name"=>"Rogelio J. Samour"},
-      'fry' => {"email"=>"crichoch@fakeinbox.com", "name"=>"Phillip J. Fry"},
-      'leela' => {"email"=>"stepriso@fakeinbox.com", "name"=>"Turanga Leela"}
+      'fry'    => {"email"=>"crichoch@fakeinbox.com", "name"=>"Phillip J. Fry"},
+      'leela'  => {"email"=>"stepriso@fakeinbox.com", "name"=>"Turanga Leela"}
     }
   end
 
-  before { Hitch::Participant.stub(:available_pairs).and_return(base_pairs) }
-
   describe ".write_file" do
-    it "writes the contents of Hitch::Participant.available_pairs to the hitch_pairs file" do
-      YAML.should_receive(:dump)
-      Hitch::Participant.write_file
+    it "writes the contents of GitPair::Participant.available_pairs to the hitch_pairs file" do
+      expect(YAML).to receive(:dump)
+      GitPair::Participant.write_file
     end
   end
 
   describe ".add" do
 
-    before { Hitch::Participant.stub(:available_pairs).and_return({}) }
-
     context "when the participant is not present" do
-      it "adds the participant to Hitch::Participant.available_pairs" do
-        Hitch::Participant.add("therubymug", "rcrichoch@fakeinbox.com", "Rogelio J. Samour")
-        expect(Hitch::Participant.available_pairs).to eql({"therubymug"=>{"email"=>"rcrichoch@fakeinbox.com", "name"=>"Rogelio J. Samour"}})
+      it "adds the participant to GitPair::Participant.available_pairs" do
+        allow(GitPair::Participant).to receive(:get_available_pairs) { {} }
+        GitPair::Participant.add("therubymug", "rcrichoch@fakeinbox.com", "Rogelio J. Samour")
+        expect(GitPair::Participant.available_pairs).to eql({"therubymug"=>{"email"=>"rcrichoch@fakeinbox.com", "name"=>"Rogelio J. Samour"}})
       end
     end
 
@@ -35,20 +32,14 @@ describe Hitch::Participant do
   describe ".find" do
 
     context "when the participant is present" do
-
-      before do
-        Hitch::Participant.stub(:available_pairs).and_return({"therubymug" => "Rogelio J. Samour"})
-      end
-
       it "and_return the full name" do
-        expect(Hitch::Participant.find("therubymug")).to eql("Rogelio J. Samour")
+        expect(GitPair::Participant.find("therubymug")).to eql({"email"=>"rcrichoch@fakeinbox.com", "name"=>"Rogelio J. Samour"})
       end
-
     end
 
     context "when the participant is not present" do
       it "and_return nil" do
-        expect(Hitch::Participant.find("nobody")).to be_nil
+        expect(GitPair::Participant.find("nobody")).to be_nil
       end
     end
 
