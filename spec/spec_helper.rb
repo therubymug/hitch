9b58b2a98a833219f25b770e512866691722bea0
Viktor Justo Vasquez <viktor@vjustov.me>
2016-07-09 20:35:40 -0400

    Adds support for a localized hitch_pairs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,6 @@ RSpec.configure do |config|
   config.before(:each) do
     Hitch.stub(:hitchrc).and_return(Tempfile.new('hitchrc').path)
     Hitch.stub(:hitch_export_authors).and_return(Tempfile.new('hitch_export_authors').path)
-    Hitch::Author.stub(:hitch_pairs).and_return(Tempfile.new('hitch_pairs').path)
+    Hitch::Author.stub(:hitch_pairs).and_return([Tempfile.new('hitch_pairs')])
   end
 end
